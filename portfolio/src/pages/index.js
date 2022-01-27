ad931300f0ab9de34a98e1dd25ddfe45c6376d8d
natacha steiver <natachasteiver@gmail.com>
2022-01-27 19:30:11 +0100

    route

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -5,15 +5,17 @@ import {AsideTop} from "../../../gatsby-theme-minimal";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 
-
+import { withPrefix } from 'gatsby'
 
 const Home=() =>{
   return (
   <>
-  
+  <Router basepath={withPrefix("/app")}>
   <AsideTop/>
   <Header/>
   <Body/>
+  </Router>
+
  
   </>
   )
